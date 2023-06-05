c8d96ee801be490a0dcdfa2a2108dfdd41f52be0
meme-castellanos <111323015+meme-castellanos@users.noreply.github.com>
2023-06-05 15:29:09 -0300

    Cambio función addProduct

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -7,14 +7,34 @@ export const CartContext = createContext({
 export const CartProvider = ({children}) => {
     const [cart, setCart] = useState ([])
 
-    const addItem = (item, amount)=>{
+    /* const addItem = (item, amount)=>{
         if (isInCart (item.id)){
             setCart(cart.map (product=> {
                 return product.id === item.id ? {product, amount : product.amount + amount}:product}));
         } else {
             setCart([...cart,{item,amount}]);
         }
-    }
+    } */
+
+    const addItem = (item, amount) => {
+        // creamos un nuevo objeto con los datos que recibimos por parámetros. Haciendo spread del producto.
+          const newObj = {...item, amount}
+        // hacer una condicional, si el nuevo objeto está en el carrito
+          if(isInCart(newObj.id)){
+        // vamos a hacer un map y sumar las cantidades, así no duplicamos
+              cart.map(el => {
+                if(el.id === newObj.id)  {
+                  el.amount+= newObj.amount
+                }
+        //retornamos 
+                return(el)
+                })
+            }
+        // si es un producto que no está en el carrito, lo va a agregar. 
+        else {
+              setCart([...cart, newObj])
+            }
+          }
 
     const removeItem = (id) => {
         const cartUpdated = cart.filter (item => item.id !== id)
