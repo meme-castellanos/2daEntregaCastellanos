90cbb8166df426cc948e23adb08b79ccfd5284c0
meme-castellanos <111323015+meme-castellanos@users.noreply.github.com>
2023-06-11 00:11:58 -0300

    estilos

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -20,7 +20,7 @@ const Checkout = () => {
   const [orderId, setOrderId] = useState("");
   const { setLoading } = useContext(GlobalContext);
   const { cart, total, clearCart } = useContext(CartContext);
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const newOrder = async ({ lastName, firstName, phone, email, adress }) => {
     setLoading(true);
     try {
@@ -63,51 +63,59 @@ const Checkout = () => {
         const orderAdded = await addDoc(orderRef, objOrder);
         setOrderId(orderAdded.id);
         clearCart();
-      } else { 
+      } else {
         Swal.fire({
-          icon: 'error',
-          position: 'top-end',
-          title: 'Oops...',
-          text: 'No tenemos suficiente stock de un producto del carrito!',
-          color:'#212529',
-          background:'#eeeeee',
+          icon: "error",
+          position: "top-end",
+          title: "Oops...",
+          text: "No tenemos suficiente stock de un producto del carrito!",
+          color: "#212529",
+          background: "#eeeeee",
           toast: true,
-          timer:3000,
-          showConfirmButton:false
-        })
+          timer: 3000,
+          showConfirmButton: false,
+        });
       }
     } catch (error) {
       Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Ha ocurrido un error, intenta nuevamente',
+        icon: "error",
+        title: "Oops...",
+        text: "Ha ocurrido un error, intenta nuevamente",
         color: "#a6be06",
-        timer: 5000
-
+        timer: 5000,
       });
     } finally {
       setLoading(false);
     }
-
-    
-  }
+  };
   if (orderId) {
-    return <div className="container-fluid bg-dark m-6" style={{height:"50vh"}}> 
-      <h1 className="text-light bg-dark m-3 mt-0">Muchas gracias por tu compra!</h1> <h3 className="text-light bg-dark m-3">El número de referencia para porsteriores consultas, es: {orderId}</h3><i className="fa-solid fa-bag-shopping fa-5x m-3" style={{color: "#a6a6a6"}}></i><button
-                className="card-link btn btn-outline-secondary m-1"
-                onClick={() => navigate('/')}
-              >
-                Volver
-              </button>
-      </div>; 
-  }
     return (
-      <div>
-        <h1>Checkout</h1>
-        <CheckoutForm onConfirm={newOrder}/>
+      <div className="container-fluid bg-dark m-6 d-flex flex-column" style={{ height: "50vh" }}>
+        <h1 className="text-light bg-dark m-3 mt-0">
+          Muchas gracias por tu compra!
+        </h1>{" "}
+        <h3 className="text-light bg-dark m-3">
+          El número de referencia para porsteriores consultas, es: {orderId}
+        </h3>
+        <i
+          className="fa-solid fa-bag-shopping fa-5x m-3"
+          style={{ color: "#a6a6a6" }}
+        ></i>
+        <button
+          className="card-link btn btn-outline-secondary m-1"
+          onClick={() => navigate("/")}
+        >
+          Volver
+        </button>
       </div>
     );
-  
-}
+  }
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <CheckoutForm onConfirm={newOrder} />
+    </div>
+  );
+};
 
 export default Checkout;
